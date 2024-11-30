99ef6f319d3f6fda40ff8f50c9b6995e932c9ac9
neddy1298 <neddy1298@gmail.com>
2024-11-30 22:56:44 +0700

    Add custom scrollbar styles and adjust spacing in the main container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 		<div className="relative">
 			<div className="fixed inset-0 z-[-2] bg-gradient-to-b from-background via-background/90 to-background" />
 
-			<div className="container mx-auto px-4 md:px-8 lg:px-16 xl:px-24 space-y-16 md:space-y-24 py-16 md:py-24">
+			<div className="container mx-auto px-4 md:px-8 lg:px-16 xl:px-24 space-y-32 md:space-y-48 py-16 md:py-24">
 				<HeroSection />
 				<AboutMe />
 				<Education />
